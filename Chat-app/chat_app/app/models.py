ddad0dbb789389de74f3e7de5b5622748f493a6d
Vivaan Shiromani <shiromanivivan@gmail.com>
2021-12-20 12:04:59 +0530

    models migrated into database

--- Chat-app/chat_app/app/models.py
+++ Chat-app/chat_app/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
+
+class Room(models.Model):
+    name = models.CharField(max_length=10000)
+class Message(models.Model):
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=10000000)
+    room = models.CharField(default="",max_length=100000)
 
 # Create your models here.
