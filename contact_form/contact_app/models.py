30c744adc652740362301235c808d49c6f7a366f
Vivaan Shiromani <shiromanivivan@gmail.com>
2022-01-01 19:11:12 +0530

    fixed bugs in blog site and added contact form submission

--- contact_form/contact_app/models.py
+++ contact_form/contact_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class contacts(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.IntegerField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+
 # Create your models here.
